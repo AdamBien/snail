2adebce6c9a90ae6cb1c60c9494f21bd7efb5259
Adam Bien <abien@adam-bien.com>
2015-12-17 20:04:38 +0100

    Migrated to ContainerRequestFilter

---
+++ snail-sample/src/test/java/com/airhacks/SnailsResourceIT.java
@@ -0,0 +1,32 @@
+package com.airhacks;
+
+import com.airhacks.rulz.jaxrsclient.JAXRSClientProvider;
+import com.airhacks.snail.rs.DelayProvider;
+import javax.ws.rs.core.Response;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class SnailsResourceIT {
+
+    JAXRSClientProvider provider = JAXRSClientProvider.buildWithURI("http://localhost:8080/snail-sample/resources/snails");
+
+    @Test
+    public void snails() {
+        final String delay = "1000";
+        Response response = provider.
+                target().
+                request().
+                header(DelayProvider.DELAY_HEADER_NAME, delay).
+                get();
+        assertThat(response.getStatus(), is(200));
+        String delayValue = response.getHeaderString(DelayProvider.DELAY_HEADER_NAME);
+        assertThat(delayValue, is(delay));
+
+    }
+
+}
